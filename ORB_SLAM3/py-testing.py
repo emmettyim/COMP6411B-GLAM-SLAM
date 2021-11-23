17a069120992ae9422bfd1444b365fe8e7def350
planetbalileua <nicoley1997@gmail.com>
2021-11-23 16:57:16 +0800

    Updated py-testing

--- ORB_SLAM3/py-testing.py
+++ ORB_SLAM3/py-testing.py
@@ -1,33 +1,22 @@
 import torch
-from models.glampoints import GLAMpoointsInference
+import numpy as np
 from PIL import Image
 import cv2
 
 if __name__ == '__main__':
-    kitti_model_path = '/home/emmett/Documents/COMP6411B/GLAMpoints_KITTI/'
-    trained_sequence = 'KITTI_10_gray' #train on kitti 00 02 03 05 06 07 08 10
-    model_name = '/model_best.pth'
-    model_path = kitti_model_path + trained_sequence + model_name
-    print("Model Path:")
-    print(model_path)
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    #model setting
-    nms = 10 # Value of the NMS window applied on the score map output of GLAMpointsInference (default:10)
-    min_prob = 0.0 #Minimum probability of a keypoint for GLAMpointsInference (default:0)
-
-    with torch.no_grad():
-        glampoints = GLAMpointsInference(path_weights=model_path, nms=nms, min_prob=min_prob)
-        model = glampoints.net
-
-        #example = torch.rand(1, 1, 376, 1241) #dim of input tensor 1x1xHxW = size of kitti image
-        #example = example.to(device)
-
-        # traced_model = torch.jit.trace(model, example)
-        img = cv2.imread("cat.png")
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        #img = cv2.resize(img, (256, 256))
-        output = model(img)
-        print(output[0][:2])
+    path = '/home/emmett/Documents/COMP6411B/GLAMpoints_KITTI/KITTI_00_gray/KITTI_00_gray.pt'
+    
+    model = torch.jit.load(path)
+    model.eval()
+    img = cv2.imread("000000.png",0)
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #img = cv2.resize(img, (256, 256))
+    #img.transform.ToTensor();
+    #img_norm = img.float()/float(image.max())
+    #img_norm = img_norm.unsqueeze(0).unsqueeze(0).float()
+    img_norm = np.float32(img) / float(np.max(img))
+    img_norm = torch.from_numpy(img_norm).unsqueeze(0).unsqueeze(0).float()
+    img_norm = img_norm.cuda()
+    output = model(img_norm)
+    print(output)
 
